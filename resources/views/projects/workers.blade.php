5c05b3c94353d1c61e24d861517b6d35a96f6d61
LuisMiguelJob <hernandezvillalazjob@gmail.com>
2023-05-12 22:35:08 -0600

    cambie todo el estilo de relacionar trabajadores, se ve mejor

--- resources/views/projects/workers.blade.php
+++ resources/views/projects/workers.blade.php
@@ -4,47 +4,46 @@
 
 @section('content')
 
-<h2>Añadir trabajadores</h2>
+<a type="button" class="btn btn-primary" href="{{ route('projects.index') }}">
+    Regresar a proyectos
+</a>
 
+<div class="col-6 col-md-4 mb-4">
+    <div class="card card-body">
 
-<div class="col-6 col-md-2 mb-4">
-<div class="card">
-    <div class="card-header pb-0">
-    <label class="ms-0">Seleccionar trabajador: </label>
-    <form action="{{ route('projects.addWorker', $project) }}" method="POST">
-        @csrf
-        @if(count($usersWithoutProject) > 0)
-            <div class="input-group input-group-static mb-4">
-            <select name="worker_id" class="form-control border-2">
-                    @foreach ($usersWithoutProject as $user)
-                            <option value="{{ $user->id }}">{{ $user->name }}</option> 
-                    @endforeach            
-            </select>
+        <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+            <h4 class="text-white mx-4">
+            Añadir trabajadores
+            </h4> 
+        </div>
+
+        <div class="card-body p-1">
+            <div class="card-header pb-0">
+                <label class="ms-0">Seleccionar trabajador: </label>
+                <form action="{{ route('projects.addWorker', $project) }}" method="POST">
+                    @csrf
+                    @if(count($usersWithoutProject) > 0)
+                        <div class="input-group input-group-static mb-4">
+                        <select name="worker_id" class="form-control border-2">
+                                @foreach ($usersWithoutProject as $user)
+                                        <option value="{{ $user->id }}">{{ $user->name }}</option> 
+                                @endforeach            
+                        </select>
+                        </div>
+            
+                    <button type="submit" class="btn btn-primary mb-4">
+                        <span>Agregar</span>
+                    </button>
+                    @else
+                        <h4>Sin opciones</h4>
+                    @endif
+                </form>
             </div>
-
-        <button type="submit" class="btn btn-primary mb-4">
-            <span>Agregar</span>
-        </button>
-        @else
-            <h4>Sin opciones</h4>
-        @endif
-    </form>
-</div>
-</div>
+        </div>
+    </div>
 </div>
 
-{{-- Esto lo dejo por si hay problemas con el codigo de arriba --}}
-{{-- <div class="input-group input-group-static mb-4">
-     <label for="exampleFormControlSelect1" class="ms-0">Example select</label>
-     <select class="form-control" id="exampleFormControlSelect1">
-       <option>1</option>
-       <option>2</option>
-       <option>3</option>
-       <option>4</option>
-       <option>5</option>
-     </select>
-   </div> --}}
-
+{{-- codigo original antes de poner estilos, dejar aqui por si hay un problema --}}
 {{-- <form action="{{ route('projects.addWorker', $project) }}" method="POST">
     @csrf
     @if(count($usersWithoutProject) > 0)
@@ -61,21 +60,68 @@
     @endif
 </form> --}}
 
-<h2>Trabajadores en "{{ $project->name }}"</h2>
-
 {{-- @foreach ($usersInProject as $uip)
     <h4>{{ $uip->name }} </h4> <button><a href={{ route('projects.removeWorker', [$project, $uip]) }}>Eliminar</a></button>
 @endforeach --}}
 
-@foreach ($usersInProject as $uip)
-    <div class="col-6 col-md-4 mb-4 px-4" style="float:left">    
-        <div class="card">
-            <div class="card-header pb-0 d-flex justify-content-center">
-                <h5>{{ $uip->name }} </h5>
-            </div>
-            <button><a href={{ route('projects.removeWorker', [$project, $uip]) }}>Eliminar</a></button>
+<div class="card my-4">
+    <div class="card-header">
+        <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
+            <h4 class="text-white mx-4">
+                Trabajadores en "{{ $project->name }}"
+            </h4> 
         </div>
     </div>
-@endforeach
+    <div class="card-body px-0 pb-2">
+        <div class="table-responsive p-0">
+            <table class="table align-items-center mb-0">
+                <thead>
+                    <tr>
+                        <th class="text-uppercase text-secondary text-x font-weight-bolder opacity-7">
+                            Nombre
+                        </th>
+                        <th class=" text-uppercase text-secondary text-x font-weight-bolder opacity-7 ps-3">
+                            Email
+                        </th>
+                        <th class=" text-uppercase text-secondary text-x font-weight-bolder opacity-7 ps-3">
+                            Rol
+                        </th>
+                        <th class="text-secondary opacity-7">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                @foreach ($usersInProject as $uip)
+                    <tr>
+                        <td>
+                              <div class="d-flex flex-column justify-content-center ps-3">
+                                  <h6 class="mb-0 text-x">{{ $uip->name }}</h6>
+                              </div>
+                        </td>
+
+                        <td>
+                            <div class="d-flex flex-column justify-content-center ps-2">
+                                <h6 class="mb-0 text-x">{{ $uip->email }}</h6>
+                            </div>
+                        </td>
+
+                        <th class=" d-flex flex-column justify-content-center ps-3">
+                            <h6 class="mb-0 text-x">{{ $uip->roles->pluck('name')->first() }}</h6>
+                        </th>
+
+                        <td class="align-middle">
+                            <a rel="tooltip" class="btn btn-primary btn-link" href={{ route('projects.removeWorker', [$project, $uip]) }} data-original-title="" title="">
+                                Borrar
+                                <div class="ripple-container"></div>
+                            </a>
+                          </td>
+
+                      </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+  </div>
 
 @endsection
